43f474144c879cf6284536af40c109571ac48f58
blackdevelopa <blackdevelopa@gmail.com>
2019-09-15 19:59:26 +0100

    added the news detail screen

--- screens/NewsScreen/NewsDetailScreen/NewsDetailScreen.js
+++ screens/NewsScreen/NewsDetailScreen/NewsDetailScreen.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import { View, Text, ScrollView, Image } from 'react-native';
+import { View, Text, ScrollView, Image, Alert } from 'react-native';
 import styles from './NewsDetailScreenStyle'
 
 export default function NewsDetailScreen() {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Deciphering Marketing Lingo For Small Business Owners</Text>
-        <Text>Small text</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerMajor}>Deciphering Marketing Lingo For Small Business Owners</Text>
+        <Text style={styles.headerMinor}>Yahoo Finance on Appl 1h</Text>
       </View>
       <View>
-        <Image />
         <ScrollView>
-          <Text>Content</Text>
+          <Image
+            style={styles.contentImage}
+            source={{
+              uri: 'https://source.unsplash.com/random'
+            }}
+          />
+          <Text style={styles.contentText} onLongPress={() => Alert.alert('Bored already?', 'You dont have to read everything you know.')}>
+            Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+            
+            The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
+            
+            Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+            
+            Various versions have evolved over the years, sometimes by accident,sometimes on purpose (injected humour and the like).
+
+            Where can I get some?
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+          </Text>
         </ScrollView>
       </View>
     </View>
--- screens/NewsScreen/NewsDetailScreen/NewsDetailScreenStyle.js
+++ screens/NewsScreen/NewsDetailScreen/NewsDetailScreenStyle.js
@@ -1,8 +1,37 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Dimensions } from 'react-native'
+import Colors from '../../../constants/Colors'
+
+let deviceWidth = Dimensions.get('window').width
 
 export default styles = StyleSheet.create({
   container: {
-    backgroundColor: '#000'
-  }
+    flex: 1,
+    backgroundColor: Colors.white
+  },
+  header: {
+    paddingVertical: 10,
+    paddingLeft: 15,
+  },
+  headerMajor: {
+    fontSize: 28,
+    fontWeight: '400',
+    textTransform: 'uppercase'
+  },
+  headerMinor: {
+    fontSize: 10,
+    fontWeight: '100'
+  },
+  contentImage: {
+    width: deviceWidth - 20,
+    height: 250,
+    marginHorizontal: 10,
+  },
+  contentText: {
+    width: deviceWidth,
+    marginTop: 20,
+    paddingHorizontal: 20,
+    textAlign: 'justify',
+    lineHeight: 30
+    }
 });
